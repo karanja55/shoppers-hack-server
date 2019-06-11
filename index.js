6a94ea69f55bbfdd4bce3c783fe5fded47157f5e
karanja55 <40261922+karanja55@users.noreply.github.com>
2019-06-11 13:12:49 +0300

    added nodemon

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const mysql =require("mysql");
 const app = express();
 const pool =mysql.createPool({
     host:"localhost",
-    user:"root",
+    user:"foo",
     password:"mufasa55",
     database:"shoppers_hack"
 
 });
 
-app.get("/",function (req, res)  {
-// logic
-
-   res.json({message:"hello clement"});
-
-
-});
 app.get("/api/suppliers",(req,res)=>{
-    pool.query("SELECT phone no,name FROM supplier",(error,rows)=>{
+    pool.query("SELECT phone_no, supplier_name FROM supplier",(error,rows)=>{
         if(error){
             return res.status(500).json({error});
         }
