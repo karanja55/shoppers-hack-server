dbe515d56b1457d03cc404640d3662bc2c43bff6
karanja55 <40261922+karanja55@users.noreply.github.com>
2019-06-11 12:10:33 +0300

    add/api/suppliers root

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express =require("express");
+const mysql =require("mysql");
 const app = express();
+const pool =mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"mufasa55",
+    database:"shoppers_hack"
+
+});
 
 app.get("/",function (req, res)  {
 // logic
@@ -7,6 +15,14 @@ app.get("/",function (req, res)  {
    res.json({message:"hello clement"});
 
 
+});
+app.get("/api/suppliers",(req,res)=>{
+    pool.query("SELECT phone no,name FROM supplier",(error,rows)=>{
+        if(error){
+            return res.status(500).json({error});
+        }
+        res.json(rows);
+    });
 });
 app.listen(9000,function(){
     console.log("app listening on port 9000");
