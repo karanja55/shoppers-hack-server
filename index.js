2f3d5d050d8f1c20fbcdc32818e56a1af60e5d4d
karanja55 <40261922+karanja55@users.noreply.github.com>
2019-06-11 14:13:25 +0300

    nodemon

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express =require("express");
 const mysql =require("mysql");
 const app = express();
 const pool =mysql.createPool({
-    host:"localhost",
-    user:"foo",
-    password:"mufasa55",
-    database:"shoppers_hack"
+    
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME
 
 });
 
