d1ba8d7c5623bbb98e672a22ce931a0eb9748cee
karanja55 <40261922+karanja55@users.noreply.github.com>
2019-06-23 20:36:21 +0300

    post

--- index.js
+++ index.js
@@ -11,66 +11,86 @@ const pool = mysql.createPool({
 
 
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+{
+    app.get("/api/suppliers", (req, res) => {
 
-app.get("/api/suppliers", (req, res) => {
-
-    pool.query("SELECT supplier_id, phone_no from supplier", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
-        }
-        res.json(rows);
-    });
-});
-
-app.get("/api/customers", (req, res) => {
-
-    pool.query("SELECT customer_id,customer_name,phone_no from customer", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
-        }
-        res.json(rows);
-    });
-});
-
-app.get("/api/buy", (req, res) => {
-
-    pool.query("SELECT customer_id,supplier_id from buy", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
-        }
-        res.json(rows);
-    });
-});
-
-//posting
-app.post("/api/suppliers:name", (req, res) => {
-
-    const supplier = req.body;
-    console.log(supplier);
-    if (!supplier.name) {
-        return res.status(400).json({ error: "invalid payload" });
-    }
-    pool.query(
-        "INSERT INTO supplier (supplier_name) values (?)",
-        [supplier.supplier_name],
-        (error, results) => {
+        pool.query("SELECT supplier_id, phone_no from supplier", (error, rows) => {
             if (error) {
                 return res.status(500).json({ error });
-
             }
-            res.json(result.insert.id);
-
+            res.json(rows);
         });
+    });
 
-    
+    app.get("/api/customers", (req, res) => {
 
-});
+        pool.query("SELECT customer_id,customer_name,phone_no from customer", (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
+        });
+    });
 
-app.listen(9000, function () {
-    console.log("app listening on port 9000");
+    app.get("/api/buy", (req, res) => {
 
-});
+        pool.query("SELECT customer_id,supplier_id from buy", (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
+        });
+    });
+
+    //posting
+    app.post("/api/suppliers:name", (req, res) => {
+
+        const supplier = req.body;
+        console.log(supplier);
+        if (!supplier.name) {
+            return res.status(400).json({ error: "invalid payload" });
+        }
+        pool.query(
+            "INSERT INTO supplier (supplier_name) values (?)",
+            [supplier.supplier_name],
+            (error, results) => {
+                if (error) {
+                    return res.status(500).json({ error });
+
+                }
+                res.json(result.insert.id);
+
+            });
+        //posting
+        app.post("/api/suppliers:name", (req, res) => {
+
+            const customer = req.body;
+            console.log(customer);
+            if (!customer.name) {
+                return res.status(400).json({ error: "invalid payload" });
+            }
+            pool.query(
+                "INSERT INTO supplier (customwer_name) values (?)",
+                [customer.customer_name],
+                (error, results) => {
+                    if (error) {
+                        return res.status(500).json({ error });
+
+                    }
+                    res.json(result.insert.id);
+
+
+
+
+
+                });
+
+            app.listen(9000, function () {
+                console.log("app listening on port 9000");
+
+            });
 
 
