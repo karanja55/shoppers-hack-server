d522d3eec8dad6c29d283d142451f7e74883db6f
karanja55 <40261922+karanja55@users.noreply.github.com>
2019-07-15 13:51:16 +0300

    navbar input

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,23 @@ import './App.css';
 
 const supplier = [
   {
-      supplier_id:'trap',
-      supplier_name:'5645676',
-      Image:'jpeg5',
-      price:5600,
-      phone_no:56542
+    supplier_id: 'trap',
+    supplier_name: '5645676',
+    Image: 'jpeg5',
+    price: 5600,
+    phone_no: 56542
 
   },
   {
-    supplier_id:'trap',
-      supplier_name:'5645676',
-      Image:'jpeg5',
-      price:5600,
-      phone_no:56542
+    supplier_id: 'trap',
+    supplier_name: '5645676',
+    Image: 'jpeg5',
+    price: 5600,
+    phone_no: 56542
 
   },
-      
-  
+
+
 ];
 
 
@@ -30,11 +30,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <navbar/>
-        <header/>
-  <p>Shoppers hack </p>
-        
+        <navbar />
+
+        <p>Shoppers hack
+    </p>
+
+
       </header>
+      
+
     </div>
   );
 }
